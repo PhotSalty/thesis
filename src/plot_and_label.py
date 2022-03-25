0a3a1e80ed4883030040c1a031fbd930249ea5b8
PhotSalty <fsaltidis@ece.auth.gr>
2022-03-26 01:21:50 +0200

    minor changes

--- src/plot_and_label.py
+++ src/plot_and_label.py
@@ -27,8 +27,8 @@ def labeling(dat, k, sbsr):
 	# axs[0].set_ylabel("g")
 	# axs[0].set_title("Accelerators 3D")
 
-names = ["gali", "sdrf", "sltn", "pasx", "anti"]
-name = names[4]
+names = ["gali", "sdrf", "sltn", "pasx", "anti", "komi"]
+name = names[5]
 datap = "C:\\Users\\30698\\Thesis_Fotis\\thesis\\data\\pickled_data\\" + name + ".pkl"
 
 with open(datap, 'rb') as f:
