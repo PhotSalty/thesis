5d03bf6128dfaa94e4e2669b36b9694d0a6fae8f
Fotis Saltidis <fsaltidis@mug6.ee.auth.gr>
2022-07-04 14:30:46 +0000

    Minor changes in testing

--- src/testing.py
+++ src/testing.py
@@ -18,7 +18,7 @@ with open(datapkl, 'rb') as f:
 ## Subject list by their tags
 subjects = np.unique(tag)
 
-s = subjects[0]
+s = subjects[8]
 test_ind = np.asarray(np.where(tag == s))[0]
 
 Test_X = windows[test_ind[0]:test_ind[-1]+1, :, :]
@@ -34,22 +34,22 @@ model = load_model(model_path)
 
 pred_Y = model.predict(x = Test_X)
 
-threshold = 0.8
-pred_Y[np.where(pred_Y < threshold)] = 0
-pred_Y[np.where(pred_Y >= threshold)] = 1
+#threshold = 0.8
+#pred_Y[np.where(pred_Y < threshold)] = 0
+#pred_Y[np.where(pred_Y >= threshold)] = 1
 
 
 # Find the local maxima between the predicted values
-p, _ = find_peaks(pred_Y[:, 0], distance = 10)
+p, _ = find_peaks(pred_Y[:, 0], distance = 15)
 print(p.shape)
 
 # Plot the comparison of the predictions with the ground truths
 plt.figure('Using alpha')
 plt.suptitle(f'Subject-{s}:',fontsize=24, y=1)
 plt.title(f'Comparing ground-truth labels with predictions',fontsize=16)
-# plt.plot(pred_Y, color = 'orange')
-plt.plot(Test_Y, color = 'blue', alpha = 1)
-plt.plot(p, pred_Y[p, 0], 'x', color = 'orange')
+plt.plot(pred_Y, color = 'orange')
+plt.plot(Test_Y, color = 'blue', alpha = 0.5)
+plt.plot(p, pred_Y[p, 0], 'x', color = 'green')
 
 # plt.plot(Test_Y, 'o')
 # plt.plot(pred_Y, 'x')
