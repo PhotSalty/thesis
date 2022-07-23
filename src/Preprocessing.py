53c9a4415c9b4b59addc1201ee175c71b727ce61
PhotSalty <fsaltidis@ece.auth.gr>
2022-07-23 14:53:13 +0300

    Functionality improvement

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -1,94 +1,61 @@
-from filterfuncs import base_stats
+
 from utils import *
 
-names = ["sltn", "gali", "sdrf", "pasx", "anti", "komi", "fot", "agge", "conp", "LH_galios"]
+names = np.array(['sltn', 'gali', 'sdrf', 'pasx', 'anti', 'komi', 'fot', 'agge', 'conp', 'LH_galios'])
 
-## Without left-handed subject
-# names = ["sltn", "gali", "sdrf", "pasx", "anti", "komi", "fot", "agge", "conp"]
 
-## Threshold selection
-# names = ['sltn', 'gali', 'sdrf', 'anti', 'conp']
-# names = ['sltn', 'sdrf']
+## Subject data processing
 
-## Print full-base statistics:
-base_stats(names, sls)
-
-## Concatenate subjects:
-subjects, ns, po, ne = subj_tot(names)
+# 1. Initialization
+subjects, ns, po, ne = subjects_init(names)
 ns = np.array(ns) - 1
 
-## Balance data - Positive-class frames augmentation:
-newsubj, ind = balance_windows(subjects, ns, po, ne)
+# 2. Balance windows - Artificial augmentation of positive-class windows"
+subjects_aug = balance_windows(subjects, ns, po, ne)
+
+# * Display base's statistical analysis - [OPTIONAL]
+display_base_stats(subjects)
 
 
-## Subjects with raw-windows
+## Extract training data
+
+# 1. Using subjects with original - raw windows
 wds_raw, lbl_raw, tg_raw, tmst_raw = concatenate_windows(subjects)
 means_raw, stds_raw = standardization_parameters(wds_raw)
 
-## Subjects with artificially-augmented-windows
-wds_aug, lbl_aug, tg_aug, tmst_aug = concatenate_windows(newsubj)
+# 2. Using subjects with balanced windows
+wds_aug, lbl_aug, tg_aug, tmst_aug = concatenate_windows(subjects_aug)
 means_aug, stds_aug = standardization_parameters(wds_aug)
 
-
-# Which standardization-parameters should we use?
-means = means_raw
-stds = stds_raw
-
-## Constructing path and save pickle:
-p = os.path.dirname(os.getcwd())
-p1 = p + sls + 'data' + sls + 'pickle_output' + sls
-
-data_raw = p1 + 'raw_data' + str(len(names)) + '_no_ssjs.pkl'
-data_aug = p1 + 'aug_data' + str(len(names)) + '_no_ssjs.pkl'
-
-with open(data_raw, 'wb') as f:
-        pkl.dump(wds_raw, f)
-        pkl.dump(lbl_raw, f)
-        pkl.dump(tg_raw, f)
-        pkl.dump(tmst_raw, f)
-        pkl.dump(means, f)
-        pkl.dump(stds, f)
-
-with open(data_aug, 'wb') as f:
-        pkl.dump(wds_aug, f)
-        pkl.dump(lbl_aug, f)
-        pkl.dump(tg_aug, f)
-        pkl.dump(tmst_aug, f)
-        pkl.dump(means, f)
-        pkl.dump(stds, f)
+# * Standardization parameters differ in those two approaches, so we need to select
+#   which "package" to use:
+means, stds = means_raw, stds_raw
 
 
-# print(f'\n\nRaw standardization parameters:\n Means = {means_raw}\n Stds = {stds_raw}')
-# print(f'\nAugmented standardization parameters:\n Means = {means_aug}\n Stds = {stds_aug}')
-# Check pickles:
+## Constructing data pickles
 
-# print(wds_raw.shape, wds_aug.shape)
+# 1. Define pickled-data path
+base_folder = os.path.dirname(os.getcwd())
+pkl_folder  = base_folder + sls + 'data' + sls + 'pickle_output' + sls
 
-# with open(data_raw, 'rb') as f:
-#       wds1 = pkl.load(f)
-#       lbl1 = pkl.load(f)
-#       tg1 = pkl.load(f)
-#       tmst1 = pkl.load(f)
+data_raw_path = pkl_folder + 'raw_data_' + str(len(names)) + '_' + file_folder + '.pkl'
+data_aug_path = pkl_folder + 'aug_data_' + str(len(names)) + '_' + file_folder + '.pkl'
 
-# fig, axs = plt.subplots(2)
-# axs[0].plot(wds1[300, :, 0])
-# axs[1].plot(subjects[0].windows[300, :, 0])
+# 2. Save pickles
+with open(data_raw_path, 'wb') as f:
+	pkl.dump(wds_raw, f)
+	pkl.dump(lbl_raw, f)
+	pkl.dump(tg_raw, f)
+	pkl.dump(tmst_raw, f)
+	pkl.dump(means, f)
+	pkl.dump(stds, f)
 
-# plt.show()
-
-
-# def comp(A, B, s):
-#       if np.array_equal(A,B):
-#               print(f'{s} Yey!')
-#       else:
-#               print(f'{s} Ney!')
-
-# ss = ['Windows', 'Labels', 'Tags', 'Timestamps']
-# comp(wds1, wds, ss[0])
-# comp(lbl1, lbl, ss[1])
-# comp(tg1, tg, ss[2])
-# comp(tmst1, tmst, ss[3])
-
-
+with open(data_aug_path, 'wb') as f:
+	pkl.dump(wds_aug, f)
+	pkl.dump(lbl_aug, f)
+	pkl.dump(tg_aug, f)
+	pkl.dump(tmst_aug, f)
+	pkl.dump(means, f)
+	pkl.dump(stds, f)
 
 
