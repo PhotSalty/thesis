982dd1449bb9421c1ada59540ed4b2c7fc8ebdcf
Fotis Saltidis <fsaltidis@mug6.ee.auth.gr>
2022-08-15 23:25:07 +0000

    changes in weights and evaluation

--- comparing_project/05_LOSO_cnn_testing.py
+++ comparing_project/05_LOSO_cnn_testing.py
@@ -42,7 +42,7 @@ for s in np.arange(10):
 
     for i in np.arange(9):
         
-        print(f'\tModels {s}_{i}\n'.expandtabs(4))
+        print(f' - Using models {s}_{i}...\n'.expandtabs(4))
         # dtree s_i model
         dtree_s_i = dtree_s[i]
 
@@ -58,32 +58,57 @@ for s in np.arange(10):
         # Windows corresponding to prediction indices
         de_wds, de_lbls, de_ind = extract_event_windows(detected_events_ind, Test_orig, Test_lbls)
 
-        print(de_wds.shape, de_lbls.shape)
+        #print(de_wds.shape, de_lbls.shape)
 
         cnn_X, cnn_Y = de_wds, de_lbls
-        pred_Y = cnn_s_i.predict(x = cnn_X)
+        cnn_pred = cnn_s_i.predict(x = cnn_X)
 
-        # Complete prediction with pre-calculated threshold -> 0.6
-        Pred_Y = np.where(pred_Y < 0.6, 0, 1)
+        # Complete prediction with pre-calculated threshold -> 0.5
+        cnn_pred_Y = np.where(cnn_pred < 0.5, 0, 1)
+        
+        # Total prediction init
+        total_pred_Y = deepcopy(pred)
+        
+        # k-index runs through cnn predictions. length( Cnn_predictions ) = length( non-zeros(tree-pred) )
+        k = 0
+
+        for j in np.arange(len(pred)):
+
+            if pred[j] == 1:
+            
+            # if the corresponding window's cnn-prediction is 0, change total prediction
+                if cnn_pred_Y[k] == 0:
+                    total_pred_Y[j] = 0
+                
+                # iterate through positive tree-predictions
+                k += 1
+
+        
+
 
         fig[fi] = plt.figure(f'Testing figure subject {s} on Model_{s}_{i}')
         fi += 1
         plt.suptitle(f'Subject <{s}>', fontsize = 24, y = 1)
         plt.title(f'Comparing ground-truth labels with predictions')
-        plt.plot(Pred_Y, color = 'orange')
-        plt.plot(cnn_Y, color = 'blue', alpha = 0.5)
+        plt.plot(total_pred_Y, color = 'orange')
+        plt.plot(Test_Y, color = 'blue', alpha = 0.5)
 
 
         # Calculate evaluation coefficients spike-per-spike
-        cm_s_i = confusion_matrix(y_true = cnn_Y, y_pred = Pred_Y)
+        #cm_s_i = confusion_matrix(y_true = cnn_Y, y_pred = Pred_Y)
+        cm_s_i = confusion_matrix(y_true = Test_Y, y_pred = total_pred_Y)
         cm_s.append(cm_s_i)
 
-        acc, prec, rec, f1s = calculate_metrics(cm = cm_s_i)
-        print_metrics(acc, prec, rec, f1s, 'Spike-per-spike')
+        #acc, prec, rec, f1s = calculate_metrics(cm = cm_s_i)
+        #print_metrics(acc, prec, rec, f1s, 'Spike-per-spike')
 
     
     # Concatenate s-subjects confusion matrices with the rest
-    cm_tot.append(np.sum(cm_s, axis = 0))
+    cm_s_temp = np.round(np.mean(cm_s, axis = 0))
+    cm_tot.append(cm_s_temp)
+
+    acc, prec, rec, f1s = calculate_metrics(cm = cm_s_temp)
+    print_metrics(acc, prec, rec, f1s, 'Spike-per-spike')
 
     # Save s-subject testing figures
     with open(fig_path + 'tstfig_' + str(s) + '.pkl', 'wb') as f:
@@ -92,7 +117,8 @@ for s in np.arange(10):
 print(f'\n> Total evaluation:\n')
 
 # 1. Spike-per-spike method:
-cm_total = np.sum(cm_tot, axis = 0)
+#cm_total = np.sum(cm_tot, axis = 0)
+cm_total = np.round(np.sum(cm_tot, axis = 0))
 Acc_tot, Prec_tot, Rec_tot, F1_tot = calculate_metrics(cm = cm_total)
 
 print_metrics(Acc_tot, Prec_tot, Rec_tot, F1_tot, 'Spike-per-spike')
