b11bf8bd042b19a12d9af912333b3302b33e3137
Fotis Saltidis <fsaltidis@mug6.ee.auth.gr>
2022-08-17 14:41:13 +0000

    returning predictions and Test_Y

--- src/testing_functions.py
+++ src/testing_functions.py
@@ -8,8 +8,8 @@ from sklearn.metrics import balanced_accuracy_score, f1_score, precision_score, 
 
 ### Display given method's metrics
 def print_metrics(Acc, Prec, Rec, F1s, mtype):
-	print(f'\n > {mtype} metrics:') 
-	print(f'''
+        print(f'\n > {mtype} metrics:') 
+        print(f'''
 \tWeighted Accuracy   ->   {Acc:.3f}
 \tPrecision           ->   {Prec:.3f}
 \tRecall              ->   {Rec:.3f}
@@ -19,113 +19,129 @@ def print_metrics(Acc, Prec, Rec, F1s, mtype):
 ### Testing subjects' models and display total results
 def LOSO_testing(windows, labels, tag, means, stds, epochs, n_subjects, mdl_path):
 
-	print(f'\n>> Testing')
+        print(f'\n>> Testing')
 
 # List of available subject tags
-	subject_tags = np.unique(tag)
+        subject_tags = np.unique(tag)
 
-	fig_path = mdl_path + 'Testing_figures' + sls
+        fig_path = mdl_path + 'Testing_figures' + sls
 
-	if not os.path.exists(fig_path):
-		os.makedirs(fig_path)
+        if not os.path.exists(fig_path):
+                os.makedirs(fig_path)
 
 # Define confusion matrices for spike-per-spike and window-per window methods
-	cm_sps = []
-	cm_wpw = []
+        cm_sps = []
+        cm_wpw = []
 
-# figure list for pickle	
-	fig = np.zeros(10, dtype = object)
-	fi = 0
+# figure list for pickle        
+        fig = np.zeros(10, dtype = object)
+        fi = 0
 
 # # Balanced accuracy total weight
-# 	is1_tot = 0
-# 	is0_tot = 0
+#       is1_tot = 0
+#       is0_tot = 0
+
+# Saving results:
+        results = np.zeros((n_subjects, 2), dtype = object)
+        ri = 0
 
 # LOSO iterations through subjects
-	for st in subject_tags:
+        for st in subject_tags:
 
-		print(f'\n > Session {st}: leaving subject <{st}> out')
+                print(f'\n > Session {st}: leaving subject <{st}> out')
 
-	# Define testing subject's indices
-		ind_tst = np.asarray(np.where(tag == st))[0]
+        # Define testing subject's indices
+                ind_tst = np.asarray(np.where(tag == st))[0]
 
-	# Define testing windows and their labels 
-		Test_X = windows[ind_tst, :, :]
-		Test_Y = labels[ind_tst]
+        # Define testing windows and their labels 
+                Test_X = windows[ind_tst, :, :]
+                Test_Y = labels[ind_tst]
 
-		Test_X = apply_standardization(windows = Test_X, means = means, stds = stds)
+                Test_X = apply_standardization(windows = Test_X, means = means, stds = stds)
 
-	# Define model path
-		model_path = mdl_path + 'M' + st + '_' + str(n_subjects) + '.mdl'
+        # Define model path
+                model_path = mdl_path + 'M' + st + '_' + str(n_subjects) + '.mdl'
 
-	# Load model and perform testing
-		model = load_model(model_path)
-		pred_Y = model.predict(x = Test_X)
+        # Load model and perform testing
+                model = load_model(model_path)
+                pred_Y = model.predict(x = Test_X)
 
-	# Complete prediction with pre-calculated threshold -> 0.6
-		Pred_Y = np.where(pred_Y < 0.6, 0, 1)
+        # Complete prediction with pre-calculated threshold -> 0.6
+                Pred_Y = np.where(pred_Y < 0.6, 0, 1)
 
-	# Find positive-class peaks, in order to eliminate multiple "positive" windows for one spike
-		p, _ = find_peaks(Pred_Y[:, 0], distance = 20)
-		pos_pred = np.asarray(np.where(Pred_Y == 1))
-		print(f'\n\tCalculated {p.shape} Peaks, out of {pos_pred.shape} positive windows'.expandtabs(4))
+        # Find positive-class peaks, in order to eliminate multiple "positive" windows for one spike
+                p, _ = find_peaks(Pred_Y[:, 0], distance = 22)
+                pos_pred = np.asarray(np.where(Pred_Y == 1))
+                print(f'\n\tCalculated {p.shape} Peaks, out of {pos_pred.shape} positive windows'.expandtabs(4))
 
-	# Construct and save a plot for the prediction results
-		fig[fi] = plt.figure('Testing figure ' + st)
-		fi += 1
-		plt.suptitle(f'Subject <{st}>', fontsize = 24, y = 1)
-		plt.title(f'Comparing ground-truth labels with predictions')
-		plt.plot(Pred_Y, color = 'orange')
-		plt.plot(Test_Y, color = 'blue', alpha = 0.5)
-		plt.plot(p, Pred_Y[p, 0], 'x', color = 'green', linewidth = 1.5)
-		# fig.savefig(fig_path + 'Subj' + st + '.pdf')
+                results[ri, 0] = Test_Y
+                results[ri, 1] = pred_Y
+                ri += 1                
 
-	# Calculate evaluation coefficients spike-per-spike
-		tp_sps, fp_sps, fn_sps, tn_sps = windows_eval_coeffs(testY = Test_Y, predY = Pred_Y, pred_peaks = p)
-		cms = np.array([[tn_sps, fp_sps], [fn_sps, tp_sps]])
-		cm_sps.append(cms)
+        # Construct and save a plot for the prediction results
+                fig[fi] = plt.figure('Testing figure ' + st)
+                fi += 1
+                plt.suptitle(f'Subject <{st}>', fontsize = 24, y = 1)
+                plt.title(f'Comparing ground-truth labels with predictions')
+                plt.plot(Pred_Y, color = 'orange')
+                plt.plot(Test_Y, color = 'blue', alpha = 0.5)
+                plt.plot(p, Pred_Y[p, 0], 'x', color = 'green', linewidth = 1.5)
+                # fig.savefig(fig_path + 'Subj' + st + '.pdf')
 
-	# Calculate evaluation coefficients window-pre-window
-		cmw = confusion_matrix(y_true = Test_Y, y_pred = Pred_Y)
-		# tn_wpw, fp_wpw, fn_wpw, tp_wpw = cmw.ravel()
-		cm_wpw.append(cmw)
+        # Calculate evaluation coefficients spike-per-spike
+                tp_sps, fp_sps, fn_sps, tn_sps = windows_eval_coeffs(testY = Test_Y, predY = Pred_Y, pred_peaks = p)
+                cms = np.array([[tn_sps, fp_sps], [fn_sps, tp_sps]])
+                cm_sps.append(cms)
 
-	# Balanced accuracy weight
-		# is1 = np.count_nonzero(Test_Y)
-		# is0 = np.shape(Test_Y)[0] - is1
-		# weight = is0 / is1
+        # Calculate evaluation coefficients window-pre-window
+                cmw = confusion_matrix(y_true = Test_Y, y_pred = Pred_Y)
+                # tn_wpw, fp_wpw, fn_wpw, tp_wpw = cmw.ravel()
+                cm_wpw.append(cmw)
 
-		acc, prec, rec, f1s = calculate_metrics(cm = cms)
-		print_metrics(acc, prec, rec, f1s, 'Spike-per-spike')
+        # Balanced accuracy weight
+                # is1 = np.count_nonzero(Test_Y)
+                # is0 = np.shape(Test_Y)[0] - is1
+                # weight = is0 / is1
 
-		acc, prec, rec, f1s = calculate_metrics(cm = cmw)
-		print_metrics(acc, prec, rec, f1s, 'Window-per-window')
-		
-		# wacc = balanced_accuracy_score(Test_Y, Pred_Y)
-		# print(f'\nWeighted accuracy = {wacc}\n')
+                acc, prec, rec, f1s = calculate_metrics(cm = cms)
+                print_metrics(acc, prec, rec, f1s, 'Spike-per-spike')
 
-		# is1_tot += is1
-		# is0_tot += is0
+                acc, prec, rec, f1s = calculate_metrics(cm = cmw)
+                print_metrics(acc, prec, rec, f1s, 'Window-per-window')
+                
+                # wacc = balanced_accuracy_score(Test_Y, Pred_Y)
+                # print(f'\nWeighted accuracy = {wacc}\n')
 
-	
-	with open(fig_path + 'tstfig.pkl', 'wb') as f:
-		pkl.dump(fig, f)
-		
-	print(f'\n Confusion matrices of {subject_tags.shape[0]} subjects')
+                # is1_tot += is1
+                # is0_tot += is0
 
-	# # Total weight
-	# weight_tot = is0_tot / is1_tot
+
+
+        
+        with open(fig_path + 'tstfig.pkl', 'wb') as f:
+                pkl.dump(fig, f)
+                
+        print(f'\n Confusion matrices of {subject_tags.shape[0]} subjects')
+        
+        # Close figures - prevent from showing
+        plt.close('all')
+
+
+        # # Total weight
+        # weight_tot = is0_tot / is1_tot
 
 # Total Confusion matrices and evaluation:
-	
-	# 1. Spike-per-spike method:
-	cm_sps_tot = np.sum(cm_sps, axis = 0)
-	Acc_sps_tot, Prec_sps_tot, Rec_sps_tot, F1_sps_tot = calculate_metrics(cm = cm_sps_tot)
-	
-	print_metrics(Acc_sps_tot, Prec_sps_tot, Rec_sps_tot, F1_sps_tot, 'Spike-per-spike')
-	
-	# 2. Winodw-per-window method:
-	cm_wpw_tot = np.sum(cm_wpw, axis = 0)
-	Acc_wpw_tot, Prec_wpw_tot, Rec_wpw_tot, F1_wpw_tot = calculate_metrics(cm = cm_wpw_tot)
+        
+        # 1. Spike-per-spike method:
+        cm_sps_tot = np.sum(cm_sps, axis = 0)
+        Acc_sps_tot, Prec_sps_tot, Rec_sps_tot, F1_sps_tot = calculate_metrics(cm = cm_sps_tot)
+        
+        print_metrics(Acc_sps_tot, Prec_sps_tot, Rec_sps_tot, F1_sps_tot, 'Spike-per-spike')
+        
+        # 2. Winodw-per-window method:
+        cm_wpw_tot = np.sum(cm_wpw, axis = 0)
+        Acc_wpw_tot, Prec_wpw_tot, Rec_wpw_tot, F1_wpw_tot = calculate_metrics(cm = cm_wpw_tot)
 
-	print_metrics(Acc_wpw_tot, Prec_wpw_tot, Rec_wpw_tot, F1_wpw_tot, 'Window-per-window')
+        print_metrics(Acc_wpw_tot, Prec_wpw_tot, Rec_wpw_tot, F1_wpw_tot, 'Window-per-window')
+
+        return results
